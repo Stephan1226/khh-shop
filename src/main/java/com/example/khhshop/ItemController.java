8f36d99814ff988d5577d80ec8feadda8677c628
Stephan Kim <hhkimstar1226@gmail.com>
2025-11-20 14:32:10 +0900

    타임리프 적용

--- src/main/java/com/example/khhshop/ItemController.java
+++ src/main/java/com/example/khhshop/ItemController.java
@@ -1,13 +1,15 @@
 package com.example.khhshop;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
     @GetMapping("/list")
-    String list() {
+    String list(Model model) {
+        model.addAttribute("name", "바지");
+        model.addAttribute("price", "15000");
         return "list.html";
     }
 }
