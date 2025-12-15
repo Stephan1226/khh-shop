ca86b2f5517514aca7dd3a99932f126f3d2d8cb4
Stephan Kim <hhkimstar1226@gmail.com>
2025-12-15 19:54:13 +0900

    Item 관련 코드 수정

--- src/main/java/com/example/khhshop/Item.java
+++ src/main/java/com/example/khhshop/Item.java
@@ -14,7 +14,15 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 200)
+    @Column(length = 255)
     private String title;
+    @Column()
     private Integer price;
+
+    public void setPrice(Integer price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price = price;
+    }
 }
